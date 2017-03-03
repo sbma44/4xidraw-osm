7ac74d54045f513487af5e0bc39591034a95cf00
Tom Lee <thomas.j.lee@gmail.com>
2017-03-02 23:46:17 -0500

    remove unused scripts

--- bin/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker build --build-arg DOWNLOAD=http://download.geofabrik.de/north-america/us/district-of-columbia-latest.osm.bz2 $(dirname $0)/..
--- bin/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -eu
-docker run -p0.0.0.0:5433:5432 -t -i $1 bash
